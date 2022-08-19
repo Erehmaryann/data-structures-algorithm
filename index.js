765b236c4555dd9efd4209c45b199d13d62428ef
Erehmaryann <edwardmaryann09@gmail.com>
2022-08-19 13:06:27 +0100

    spinal tap case and pig latin algorithm

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
 // For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
-
 // soln
 const sumAllNumsInRange = (arr) => {
   let min = Math.min(...arr);
@@ -17,13 +16,99 @@ sumAllNumsInRange([4, 1]);
 
 // Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
 // Note: You can return the array with its elements in any order.
-
 // soln
 const diffArray = (arr1, arr2) => {
   let bothArr = arr1.concat(arr2);
   return bothArr.filter((item) => !arr1.includes(item) || !arr2.includes(item));
 };
-
 diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
 // The time complexity of the above func is linear O(n)
 // The space complexity of the above func is constant O(n)
+
+// Seek and Destroy.
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+// Note: You have to use the arguments object.
+// soln
+const removeElemsOfSameVal = (arr, ...args) => {
+  let allElemsArr = arr.concat(args);
+  return allElemsArr.filter(
+    (elem) => !arr.includes(elem) || !args.includes(elem)
+  );
+};
+removeElemsOfSameVal([1, 2, 3, 1, 2, 3], 2, 3); // [1, 1]
+// The time complexity of the above func is linear O(n)
+// The space complexity of the above func is constant O(n)
+
+// Wherefore art thou.
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+// For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+// soln
+const CheckForMatchedNameAndVal = (collection, source) => {
+  let sourceKeys = Object.keys(source);
+  const arrOfMatchedName = collection.filter((item) =>
+    sourceKeys.every(
+      (key) => item.hasOwnProperty(key) && item[key] === source[key]
+    )
+  );
+  return arrOfMatchedName;
+};
+CheckForMatchedNameAndVal(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" },
+  ],
+  { last: "Capulet" }
+); // [{ first: "Tybalt", last: "Capulet" }]
+
+// Spinal Tap Case.
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+// soln
+const spinalCase = (string) => {
+  string = string
+    .split(/\s|_|(?=[A-Z])/)
+    .join("-")
+    .toLowerCase();
+  return string;
+};
+
+// Pig Latin.
+// Pig Latin is a way of altering English Words. The rules are as follows:
+// - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
+// - If a word begins with a vowel, just add way at the end.
+// Translate the provided string to Pig Latin. Input strings are guaranteed to be English words in all lowercase.
+// soln
+const translatePigLatin = (str) => {
+  const consonants = str.match(/[^aeiou]/gi);
+  if (consonants.includes(str[0])) {
+    str = [...str];
+    let wordsToBeRemoved = "";
+    let arr = [];
+    for (let i = 0; i <= str.length - 1; i++) {
+      let cluster = consonants.includes(str[i]);
+      if (cluster) {
+        wordsToBeRemoved += str[i];
+        arr = [...wordsToBeRemoved];
+      } else if (wordsToBeRemoved.length === 1) {
+        let p = str.shift();
+        str.push(p);
+        return str.join("") + "ay";
+      } else if (!cluster) {
+        break;
+      }
+    }
+    if (wordsToBeRemoved.length > 1) {
+      let bothArr = str.concat(arr);
+      let remArr = bothArr.filter(
+        (elem) => !arr.includes(elem) || !str.includes(elem)
+      );
+      return remArr.join("") + arr.join("") + "ay";
+    }
+  } else {
+    return str + "way";
+  }
+  return str;
+};
+
+translatePigLatin("algorithm"); // algorithmway
+translatePigLatin("paragraphs"); // aragraphspay
