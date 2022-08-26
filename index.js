ef8f557dd2ca0216d282dede4267fcaa608b4006
Erehmaryann <edwardmaryann09@gmail.com>
2022-08-26 15:17:19 +0100

    refactor search and replace

--- index.js
+++ index.js
@@ -120,4 +120,23 @@ translatePigLatin("paragraphs"); // aragraphspay
 // Third argument is what you will be replacing the second argument with (after).
 // Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
 // soln
-const searchAndReplace = (str, before, after) => {};
+const searchAndReplace = (string, before, after) => {
+  let afterFirstChar = after.charAt(0);
+  let beforeIndex = string.indexOf(before);
+  let beforeFirstChar = string.charAt(beforeIndex);
+  if (beforeFirstChar == beforeFirstChar.toLowerCase()) {
+    return string.replace(
+      before,
+      afterFirstChar.toLowerCase() + after.slice(1)
+    );
+  } else if (beforeFirstChar == beforeFirstChar.toUpperCase()) {
+    return string.replace(
+      before,
+      afterFirstChar.toUpperCase() + after.slice(1)
+    );
+  }
+  return string;
+};
+// Time complexity is linear-O(n)
+searchAndReplace("Let us go to the store", "store", "mall"); //Let us go to the mall
+searchAndReplace("He is Sleeping on the couch", "Sleeping", "sitting"); //He is Sitting on the couch.
