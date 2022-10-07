4469e6a68bc9210a07a936e12d25c6a6d06cea44
Erehmaryann <edwardmaryann09@gmail.com>
2022-10-07 17:25:47 +0100

    missing letters

--- index.js
+++ index.js
@@ -162,5 +162,30 @@ const pairElements = (str) => {
 
   return arr;
 };
-// pairElement("GCG"); [["G", "C"], ["C","G"], ["G", "C"]]
+pairElements("GCG"); //[["G", "C"], ["C","G"], ["G", "C"]]
 // Time complexity is linear-O(n)
+
+// Missing letters
+// Find the missing letter in the passed letter range and return it.
+// If all letters are present in the range, return undefined.
+// soln
+const Ref = "abcdefghijklmnopqrstuvwxyz";
+
+const missingLetters = (str) => {
+  let start = str[0];
+  let end = str[str.length - 1];
+  let range = Ref.slice(Ref.indexOf(start), Ref.indexOf(end) + 1);
+
+  let f;
+  for (let i = 0; i < range.length; i++) {
+    if (str.includes(range.charAt(i))) {
+      continue;
+      f = undefined;
+    } else {
+      f = range.charAt(i);
+    }
+  }
+  return f;
+};
+
+missingLetters("abce"); //"d"
