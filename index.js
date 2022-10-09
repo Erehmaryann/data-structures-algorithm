73224d067860d8b3df68b6a947df2fd7c8ceb28d
Erehmaryann <edwardmaryann09@gmail.com>
2022-10-09 17:00:14 +0100

    missing letters soln 2

--- index.js
+++ index.js
@@ -168,24 +168,42 @@ pairElements("GCG"); //[["G", "C"], ["C","G"], ["G", "C"]]
 // Missing letters
 // Find the missing letter in the passed letter range and return it.
 // If all letters are present in the range, return undefined.
-// soln
-const Ref = "abcdefghijklmnopqrstuvwxyz";
+// soln 1 time complexity is O(n2) quadratic
+// const Ref = "abcdefghijklmnopqrstuvwxyz";
 
+// const missingLetters = (str) => {
+//   let start = str[0];
+//   let end = str[str.length - 1];
+//   let range = Ref.slice(Ref.indexOf(start), Ref.indexOf(end) + 1);
+
+//   let f;
+//   for (let i = 0; i < range.length; i++) {
+//     if (str.includes(range.charAt(i))) {
+//       continue;
+//       f = undefined;
+//     } else {
+//       f = range.charAt(i);
+//     }
+//   }
+//   return f;
+// };
+
+//soln2
 const missingLetters = (str) => {
-  let start = str[0];
-  let end = str[str.length - 1];
-  let range = Ref.slice(Ref.indexOf(start), Ref.indexOf(end) + 1);
+  let previousCharASCII = str.charCodeAt(0);
 
-  let f;
-  for (let i = 0; i < range.length; i++) {
-    if (str.includes(range.charAt(i))) {
-      continue;
-      f = undefined;
-    } else {
-      f = range.charAt(i);
+  for (let i = 1; i < str.length; i++) {
+    // charCodeAt() converts strings to unicode values.
+    let currentCharASCII = str.charCodeAt(i);
+
+    if (currentCharASCII !== previousCharASCII + 1) {
+      // String.fromCharCode() converts Unicode values to strings.
+      return String.fromCharCode(previousCharASCII + 1);
     }
+
+    previousCharASCII = currentCharASCII;
   }
-  return f;
 };
 
 missingLetters("abce"); //"d"
+//time complexity-O(n)- linear.
